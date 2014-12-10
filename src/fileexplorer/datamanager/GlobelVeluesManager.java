1f746d2df08c52bec5aae8134d859619a643ffec
enphoneh <362837428@qq.com>
2014-12-11 00:09:12 +0800

    增加全局变量类，控制全局变量

---
+++ src/fileexplorer/datamanager/GlobelVeluesManager.java
@@ -0,0 +1,13 @@
+package fileexplorer.datamanager;
+
+public class GlobelVeluesManager {
+
+    final public static  int TITLEBAR_LEFTBUTTON_INDEX =  0;
+    final public static  int TITLEBAR_CENTERTITLE_INDEX = 2;
+    final public static  int TITLEBAR_RIGHTBUTTON_INDEX = 1;
+    
+	public GlobelVeluesManager() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
